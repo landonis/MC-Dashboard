a776dc5a529c386212579764d4fcccbc955577b3
Landon Diedrich <43480252+landonis@users.noreply.github.com>
2025-08-05 16:28:25 -0400

    Update GroupManager.java

--- dashboard-mod/src/main/java/net/landonis/dashboardmod/GroupManager.java
+++ dashboard-mod/src/main/java/net/landonis/dashboardmod/GroupManager.java
@@ -9,7 +9,8 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.world.storage.WorldSavePath;
+import net.minecraft.util.WorldSavePath;
+
 public class GroupManager {
     private static final Map<String, Group> groups = new HashMap<>();
     private static final Gson gson = new Gson();
